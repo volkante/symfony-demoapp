51ab88cdb4d217a84dc3e2c131bf3b7d492822c8
volkan t <volkan.temelli@gmail.com>
2025-12-29 19:38:09 +0100

    feat: add save button to ProductType form and update new product template

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -6,6 +6,7 @@ use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ProductType extends AbstractType
 {
@@ -15,6 +16,7 @@ class ProductType extends AbstractType
             ->add('name')
             ->add('description')
             ->add('size')
+            ->add('save', SubmitType::class, ['label' => 'Add New Product'])
         ;
     }
 
